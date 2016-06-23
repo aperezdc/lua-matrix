ec288bf9fd2a576e427600fa38a00ef9a602cab4
Adrian Perez de Castro <aperez@igalia.com>
2016-06-23 03:55:09 +0300

    Make it possible to import "matrix" directly
    This aliases "matrix.client", making both the following equivalent:
    
      matrix = require "matrix.client"
      matrix = require "matrix"

---
+++ matrix.lua
@@ -0,0 +1,8 @@
+#! /usr/bin/env lua
+--
+-- matrix.lua
+-- Copyright (C) 2016 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+return require "matrix.client"
